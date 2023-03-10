a5ade7facbdafd0da2257861f4b5047f53f3e573
Christian <christian.schindler@tum.de>
2023-03-10 22:55:21 +0100

    Fynn

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,71 @@
 #include <Arduino.h>
-
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEScan.h>
+#include <BLEAdvertisedDevice.h>
+String knownBLEAddresses[] = {"90:81:75:4d:04:1A","DC:DC:E2:19:AB:E3","A8:9C:ED:45:CA:C0"};
+int RSSI_THRESHOLD = -55;
+bool device_found;
+int scanTime = 5; //In seconds
+BLEScan* pBLEScan;
+class MyAdvertisedDeviceCallbacks: public BLEAdvertisedDeviceCallbacks {
+    void onResult(BLEAdvertisedDevice advertisedDevice) {
+      for (int i = 0; i < (sizeof(knownBLEAddresses) / sizeof(knownBLEAddresses[0])); i++)
+      {
+        //Uncomment to Enable Debug Information
+        //Serial.println("*************Start**************");
+        //Serial.println(sizeof(knownBLEAddresses));
+        //Serial.println(sizeof(knownBLEAddresses[0]));
+        //Serial.println(sizeof(knownBLEAddresses)/sizeof(knownBLEAddresses[0]));
+        //Serial.println(advertisedDevice.getAddress().toString().c_str());
+        //Serial.println(knownBLEAddresses[i].c_str());
+        //Serial.println("*************End**************");
+        if (strcmp(advertisedDevice.getAddress().toString().c_str(), knownBLEAddresses[i].c_str()) == 0)
+                        {
+          device_found = true;
+                          break;
+                        }
+        else
+          device_found = false;
+      }
+      Serial.printf("Advertised Device: %s \n", advertisedDevice.getAddress().toString().c_str());  //only adresse
+      Serial.printf("Advertised Device: %s \n", advertisedDevice.toString().c_str());  // random bullshit + adresse
+}
+};
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200); //Enable UART on ESP32
+  Serial.println("Scanning..."); // Print Scanning
+
+  pinMode(2, OUTPUT); //make BUILTIN_LED pin as output
+  BLEDevice::init("");
+  pBLEScan = BLEDevice::getScan(); //create new scan
+  pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks()); //Init Callback Function
+  pBLEScan->setActiveScan(true); //active scan uses more power, but get results faster
+  pBLEScan->setInterval(100); // set Scan interval
+  pBLEScan->setWindow(99);  // less or equal setInterval value
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  BLEScanResults foundDevices = pBLEScan->start(scanTime, false);
+  for (int i = 0; i < foundDevices.getCount(); i++)
+  {
+    BLEAdvertisedDevice device = foundDevices.getDevice(i);
+    int rssi = device.getRSSI();
+   Serial.print("RSSI: ");
+   Serial.println(rssi);
+    if (rssi > RSSI_THRESHOLD && device_found == true)
+    {
+      digitalWrite(2, HIGH);
+      Serial.print("JAAAAAAAAAAAAAAA");
+    }
+    else
+    {
+       digitalWrite(2, LOW);
+       Serial.print("NOOOOOOOOOOOOOO");
+    }
+     
+  }
+  pBLEScan->clearResults();   // delete results fromBLEScan buffer to release memory
+  delay(200);
 }
