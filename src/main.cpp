7f40c7e1441411a19b322d431b3f43ccd333aa0c
Fynno <f.gewiese@gmx.de>
2023-03-18 11:25:34 +0100

    FriedrichServer <3

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,18 @@
 #include "NimBLEDevice.h"
 
 void setup() {
-    NimBLEDevice::init("NimBLE");
+  NimBLEDevice::init("Hallo_ich_bin_Fredderich");
+    
     NimBLEServer *pServer = NimBLEDevice::createServer();
     NimBLEService *pService = pServer->createService("ABCD");
+    NimBLECharacteristic *pCharacteristic = pService->createCharacteristic("1234");
+    
+    pService->start();
+    pCharacteristic->setValue("Hello BLE");
+    
+    NimBLEAdvertising *pAdvertising = NimBLEDevice::getAdvertising();
+    pAdvertising->addServiceUUID("ABCD"); 
+    pAdvertising->start(); 
 } // End of setup.
 
 
